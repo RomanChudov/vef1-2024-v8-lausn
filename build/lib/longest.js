749f4875796f37418a03b1f944c5926ec8cc1a9a
RomanChudov <rc3670h@gmail.com>
2024-10-24 22:28:38 +0000

    ekki enn

---
+++ build/lib/longest.js
@@ -0,0 +1,23 @@
+// Longest
+
+import { isString, splitOnWhitespace } from "./helpers.js";
+
+export function longest(str) {
+  if (isString(str)) {
+    const stru = splitOnWhitespace(str);
+    var i = 0;
+    for (var x in stru) {
+      if (stru[x].length > stru[i].length) {
+        i = x;
+      }
+    }
+    return stru[i];
+  } else {
+    return null;
+  }
+}
+
+console.assert(longest("halló hæ") === "halló", "5 er stærri en 2");
+console.assert(longest("halló hæ bææææ") === "halló", "fysrtur kemur first");
+console.assert(longest("") === "", "tóm gefur tóm");
+console.assert(longest(true) === null, "ekki streng gefur null");
---
+++ build/lib/start.js
@@ -0,0 +1,53 @@
+// Leiðbeint ferli
+import { isString, splitOnWhitespace } from "./helpers.js";
+
+import { consonants } from "./consonants.js";
+import { vowels } from "./vowels.js";
+import { longest } from "./longest.js";
+import { shortest } from "./shortest.js";
+import { palindrome } from "./palindrome.js";
+import { reverse } from "./reverse.js";
+
+export function start() {
+  // Útfæra
+  alpha = 0;
+  alert(
+    "skrifa niður streng og sjá eiginleika þess:" +
+      "\n longest(strengur) til að skila lengsta orði í streng." +
+      "\n shortest(strengur) til að skila stysta orði í streng." +
+      "\n reverse(strengur) skilar öfugum streng." +
+      "\n vowels(strengur) skilar fjölda sérhljóða í streng." +
+      "\n consonant(strengur) skilar fjölda samhljóða í streng." +
+      "\n palindrome(strengur) til að athuga hvort strengur sé samhverfur."
+  );
+
+  while (alpha === 0) {
+    const stra = prompt("Skrifaðu hér streng", "");
+    if (isString(stra) && stra != "") {
+      alert(
+        "streng er: " +
+          stra +
+          "\nlengsta orðið er: " +
+          longest(stra) +
+          "\nstyrsta orðið er: " +
+          shortest(stra) +
+          "\nöfug streng: " +
+          reverse(stra) +
+          "\nfjölda sérhljóða: " +
+          vowels(stra) +
+          "\nfjölda samhljóða: " +
+          consonants(stra) +
+          "\ner strengur samhverfur: " +
+          palindrome(stra)
+      );
+    } else {
+      alpha = 2;
+    }
+    if (alpha != 2) {
+      var afram = confirm("Halda áfram?", "");
+      if (afram === false) {
+        alpha = 1;
+      }
+    }
+  }
+}
